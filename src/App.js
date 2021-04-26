d05fbf4be4da65685bcad6805ec921ee64cfcaf1
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-04-26 18:18:25 -0500

    create search form, store keys in process.env

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const APP_ID = `${process.env.REACT_APP_APP_ID}`
+  const APPLICATION_KEY = `${process.env.REACT_APP_APPLICATION_KEY}`
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APPLICATION_KEY}`
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <form className='search-form'>
+        <input className='search-bar' type='text'/>
+        <button className='search-button' type='submit'>Search</button>
+      </form>
     </div>
   );
 }
