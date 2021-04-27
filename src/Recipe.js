4633c9003f58532b6e02799ab6de5b51fff5bb37
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-04-27 10:27:26 -0500

    style recipe component

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function Recipe({title, calories, image, ingredients}) {
     return (
         <div className='recipe-container'>
-            <img src={image} alt=""/>
-            <h1 className='title'>{title}</h1>
-            <ul className='ingredient-list'>
+            <img className='recipe-image' src={image} alt=""/>
+            <h1 className='recipe-title'>{title}</h1>
+            <ul className='recipe-ingredient-list'>
                 {ingredients.map(ingredient => (
-                    <li className='ingredient'>{ingredient.text}</li>
+                    <li className='recipe-ingredient'>{ingredient.text}</li>
                 ))}
             </ul>
-            <p className='calories'>Calories: {calories.toFixed(0)}</p>
+            <p className='recipe-calories'>Calories: {calories.toFixed(0)}</p>
         </div>
     )
 }
